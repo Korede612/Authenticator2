dc0dc7dd43633fb73ff170ffbfb0299badcb2b12
Oko-Osi Korede <korede.oko-osi@fcmb.com>
2025-05-06 02:54:04 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Authenticator2",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Authenticator2",
+            targets: ["Authenticator2"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Authenticator2"),
+        .testTarget(
+            name: "Authenticator2Tests",
+            dependencies: ["Authenticator2"]
+        ),
+    ]
+)
